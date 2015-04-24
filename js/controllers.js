9b92e74852522110e47ff723221d6ed9b9599e32
juliahw <juliahw@princeton.edu>
2015-04-24 17:13:45 -0400

    Added content

--- js/controllers.js
+++ js/controllers.js
@@ -18,11 +18,52 @@ app.controller('homeController', ['$scope', function ($scope) {
 }]);
 
 app.controller('productController', ['$scope', function ($scope) {
+    $scope.index = 0;
+    $scope.screens = [
+        {
+            url: '../img/screens/screen1.png',
+            description: 'Download Clutch to get access to ALL retailers using our product.'
+    },
+        {
+            url: '../img/screens/screen1.png',
+            description: 'Immediately see existing deals.'
+    },
+        {
+            url: '../img/screens/screen1.png',
+            description: 'As you browse, personalized coupons will be served for items you want.'
+    },
+        {
+            url: '../img/screens/screen2.png',
+            description: 'Treat our coupons like any other coupon. Present it at the sales counter and save!'
+    }];
+
+    $scope.showNextScreen = function () {
+        $scope.index = ($scope.index + 1) % $scope.screens.length;
+    }
+
+    $scope.showPrevScreen = function () {
+        $scope.index = ($scope.index - 1) % $scope.screens.length;
+    }
+
+    $scope.hasNextScreen = function () {
+        return $scope.index < $scope.screens.length - 1;
+    }
+
+    $scope.hasPrevScreen = function () {
+        return $scope.index > 0;
+    }
+
     $('#pg-product').fadeIn();
 }]);
 
 app.controller('shoppersController', ['$scope', function ($scope) {
     $('#pg-shoppers').fadeIn();
+
+    $scope.showMore = function () {
+        $('.bg-img, #shoppers-learn-btn').fadeOut(500);
+        $('#shoppers-signup-btn').delay(500).fadeIn(500);
+        $('#shoppers-info').slideDown(1000);
+    };
 }]);
 
 app.controller('retailersController', ['$scope', function ($scope) {
@@ -33,7 +74,6 @@ app.controller('retailersController', ['$scope', function ($scope) {
         $('#retailers-signup-btn').delay(500).fadeIn(500);
         $('#retailers-info').slideDown(1000);
     };
-
 }]);
 
 app.controller('detailsController', ['$scope', function ($scope) {
